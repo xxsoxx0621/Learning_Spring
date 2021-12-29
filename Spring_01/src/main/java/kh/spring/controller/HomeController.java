082d8af4ebca434953cc6eef51e69680724de5eb
baesohyeon <baesohyeon@218.38.137.27>
2021-12-29 21:18:50 +0900

    Day01

---
+++ Spring_01/src/main/java/kh/spring/controller/HomeController.java
@@ -0,0 +1,155 @@
+package kh.spring.controller;
+
+
+
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import kh.spring.dao.ContactDAO;
+import kh.spring.dto.ContactDTO;
+
+
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	public ContactDAO dao;
+			
+	// HttpSession 객체를 가져와서 사용 할 수 있게 만들어줌
+	@Autowired
+	private HttpSession session;
+	
+	@RequestMapping("/")
+	public String home() {
+	
+		System.out.print("/로 들어온 요청은 이 메서드를 실행합니다.");
+		
+		
+		// return 값은 세가지만 가능하다.
+		// void
+		// String
+		// ModelAndView
+		
+		return "home";
+		
+		//RequestMapping 메서드에서 return 값은 default가 foward이다.
+	}
+	@RequestMapping("toInput")
+	public String toInput()  {
+		System.out.println("toInput으로 가는 메서드가 실행되었습니다.");
+		return "inputForm";
+	}
+	
+//	@RequestMapping("inputProc")
+//	public String inputProc(String name, String contact) {
+//		
+////		String name = request.getParameter("name");
+////		String contact = request.getParameter("contact");
+//		System.out.println(name + " : " + contact);
+////		System.out.println("데이터 잘 도착!");
+//		return "home";
+//	}
+//	
+	
+//	// 만약 session 값의 로그인 아이디를 가져오고 싶으면 매개변수로 넣어주면 된다.
+//	@RequestMapping("inputProc")
+//	public String inputProc(ContactDTO dto, HttpSession session) {
+//		
+//	
+//		System.out.println(dto.getName() + " : " + dto.getContact());
+//		
+//		String id = (String)session.getAttribute("loginId");
+//
+//		return "home";
+//	}
+	// 만약 session 값의 로그인 아이디를 가져오고 싶으면 매개변수로 넣어주면 된다.
+	
+	
+	@RequestMapping("inputProc")
+	public String inputProc(ContactDTO dto) throws Exception {
+	
+			int result = dao.insert(dto);
+
+//		return "home";
+		return "redirect:/";
+	}
+	
+	// 스프링에 foward가 redirect로 페이지를 이동해야 할 경우 어떻게 하는가?
+	
+//	목록을 담아 JSP로 보내는 방법 1	
+//	@RequestMapping("outputProc")
+//	public ModelAndView outputProc() {
+//		ModelAndView mav = new ModelAndView();
+//		
+//		try {
+//			List<ContactDTO> list = dao.selectAll();
+//			mav.addObject("list",list);
+//			mav.setViewName("output");
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//			return "error";
+//		}
+//
+//		return mav;
+//	}
+//	}
+	
+
+//	목록을 담아 JSP로 보내는 방법 2	
+		@RequestMapping("toOutput")
+		public String outputProc(Model model)throws Exception {
+			
+	
+			List<ContactDTO> list = dao.selectAll();
+			int count = dao.selectCount();
+			model.addAttribute("list", list);
+			model.addAttribute("count", count);
+	
+			return "output";
+		}
+		
+		@RequestMapping("deleteProc")
+		public String deleteProc(int delTarget)throws Exception {
+			
+			int result = dao.delete(delTarget);
+	
+			return "redirect:toOutput";
+		}
+		@RequestMapping("updateProc")
+		public String updateProc(ContactDTO dto) throws Exception{
+			int result = dao.update(dto);
+			return "redirect:toOutput";
+		}
+		
+		@RequestMapping("search")
+		public String search(int searchSeq,Model model) throws Exception{
+			List<ContactDTO> list = dao.search(searchSeq);
+			model.addAttribute("list",list);
+			return "output";
+		}
+		// 예외의 종류를 나눌 수 있음.
+		@ExceptionHandler(Exception.class)
+		public String exceptionHandler(Exception e) {
+			e.printStackTrace();
+			System.out.println("예외 코드가 실행되었습니다.");
+			return "redirect:/";
+			
+		}
+//		@ExceptionHandler(NumberFormatException.class)
+//		public String exceptionHandler1() {
+//			System.out.println("예외 코드가 실행되었습니다.");
+//			return "home";
+//			
+//		}
+}
---
+++ Spring_01/src/main/java/kh/spring/dao/ContactDAO.java
@@ -0,0 +1,163 @@
+package kh.spring.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import kh.spring.dto.ContactDTO;
+
+@Component		
+public class ContactDAO {
+	
+	// DBCP가 세팅이 되어 있기 때문에 DBCP를 직접 사용하는 것과 동일하다.
+	@Autowired
+	private JdbcTemplate jdbc;
+	
+	
+	public int insert(ContactDTO dto) throws Exception{
+		
+		String sql = "insert into contact values(contact_seq.nextval,?,?)";
+		return jdbc.update(sql,dto.getName(),dto.getContact());
+		
+	}
+	public int delete(int seq) throws Exception {
+		String sql = "delete from contact where seq = ?";
+		return jdbc.update(sql, seq);
+		
+	}
+	public int update(ContactDTO dto) throws Exception{
+		String sql ="update contact set name =?, contact =? where seq = ?";
+		return jdbc.update(sql, dto.getName(),dto.getContact(),dto.getSeq());
+	}
+	public int selectCount() throws Exception{
+		String sql = "select count(*) from contact";
+		return jdbc.queryForObject(sql, Integer.class);
+	}
+	
+	public List<ContactDTO> search(int seq) throws Exception{
+		String sql = "select * from contact where seq = ?";
+		
+		
+		return jdbc.query(sql, new RowMapper<ContactDTO>(){
+			@Override
+			public ContactDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+				
+				ContactDTO dto = new ContactDTO();
+				dto.setSeq(rs.getInt("seq"));
+				dto.setName(rs.getString("name"));
+				dto.setContact(rs.getString("contact"));
+				return dto;
+				
+			}
+		},seq);
+		
+		// 하나의 데이터를 검색하는게 확정되는 경우 (ex: 마이페이지 or 내 정보 보기)
+//		return jdbc.queryForObject(sql, new RowMapper<ContactDTO>() {
+//
+//			@Override
+//			public ContactDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+//				ContactDTO dto = new ContactDTO();
+//				dto.setSeq(rs.getInt("seq"));
+//				dto.setName(rs.getString("name"));
+//				dto.setContact(rs.getString("contact"));
+//				return dto;
+//			}
+//			
+//		},seq);
+	}
+	public List<ContactDTO> selectAll() throws Exception{
+		
+		String sql = "select * from contact";
+		
+		return jdbc.query(sql, new RowMapper<ContactDTO>(){
+			@Override
+			public ContactDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+				
+				ContactDTO dto = new ContactDTO();
+				dto.setSeq(rs.getInt("seq"));
+				dto.setName(rs.getString("name"));
+				dto.setContact(rs.getString("contact"));
+				return dto;
+				
+			}
+		});
+		
+
+	
+		// jdbc.query :  List 데이터를 가져올 때 사용
+		// jdbc.queryForObject : 한개의 데이터를 가져 올 때, 사용한다. ( DTO, int) 
+		
+	}
+//	public int insert(ContactDTO dto) throws Exception {
+//		
+//		String sql = "insert into contact values(contact_seq.nextval,?,?)";
+//		try(Connection con = bds.getConnection();
+//		PreparedStatement pstat = con.prepareStatement(sql);){
+//			pstat.setString(1, dto.getName());
+//			pstat.setString(2, dto.getContact());
+//			return pstat.executeUpdate();
+//			
+//		}
+//	}
+//	
+//	public List<ContactDTO> selectAll() throws Exception{
+//		String sql = "select * from contact";
+//		try(Connection con = bds.getConnection();
+//				PreparedStatement pstat = con.prepareStatement(sql);
+//				ResultSet rs = pstat.executeQuery();){
+//			
+//			List<ContactDTO> list = new ArrayList<>();
+//			while(rs.next()) {
+//				int seq = rs.getInt("seq");
+//				String name = rs.getString("name");
+//				String contact = rs.getString("contact");
+//				
+//				list.add(new ContactDTO(seq,name,contact));
+//			}
+//			return list;
+//		}
+//		
+//	}
+//	public int delete(int num) throws Exception{
+//		
+//		String sql = "delete from contact where seq = ?";
+//		
+//		try(Connection con = bds.getConnection();
+//				PreparedStatement pstat = con.prepareStatement(sql);){
+//			
+//				pstat.setInt(1, num);
+//				int result = pstat.executeUpdate();
+//				return result;
+//			}
+//			
+//		}
+//	
+//	public int update(ContactDTO dto) throws Exception{
+//		
+//		String sql ="update contact set name =?, contact =? where seq = ?";
+//		
+//		try(Connection con = bds.getConnection();
+//				PreparedStatement pstat = con.prepareStatement(sql);){
+//			
+//			
+//			pstat.setString(1, dto.getName());
+//			pstat.setString(2, dto.getContact());
+//			pstat.setInt(3, dto.getSeq());
+//			
+//			int result = pstat.executeUpdate();
+//			return result;
+//		}
+//		
+//	}
+	}
+	
+
